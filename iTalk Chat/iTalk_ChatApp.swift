a6b13a862a3c8112ef283c8e81b1eea5ee281250
Swift Build <swiftbuild94@gmail.com>
2022-03-27 19:41:32 -0300

    Initial Commit

---
+++ iTalk Chat/iTalk_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTalk_ChatApp.swift
+//  iTalk Chat
+//
+//  Created by Patricio Benavente on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iTalk_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
