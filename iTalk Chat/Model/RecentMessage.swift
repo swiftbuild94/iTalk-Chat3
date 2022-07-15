5e552fa9445ec8e602f92f921a20b68683da4158
Patricio Benavente <patricio.benavente@katoo.app>
2022-07-15 06:50:32 -0300

    Recent Messages

--- iTalk Chat/Model/RecentMessage.swift
+++ iTalk Chat/Model/RecentMessage.swift
@@ -12,7 +12,9 @@ import FirebaseFirestoreSwift
 struct RecentMessage: Identifiable, Codable, Hashable {
     @DocumentID var id: String?
     let fromId, toId: String
-    let text: String
+    let text: String?
+    let audioTimer: Double?
+    let photo: String?
     let timestamp: Date
     var timeAgo: String {
         let formater = RelativeDateTimeFormatter()
@@ -23,7 +25,8 @@ struct RecentMessage: Identifiable, Codable, Hashable {
     init(data: [String:Any]){
         self.fromId = data[FirebaseConstants.fromId] as? String ?? ""
         self.toId = data[FirebaseConstants.toId] as? String ?? ""
-        
+        self.audioTimer = data[FirebaseConstants.audioTimer] as? Double
+        self.photo = data[FirebaseConstants.photo] as? String ?? ""
         self.text = data[FirebaseConstants.text] as? String ?? ""
         self.timestamp = data[FirebaseConstants.timestamp] as? Date ?? Date()
     }
