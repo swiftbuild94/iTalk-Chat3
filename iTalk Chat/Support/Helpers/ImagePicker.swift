afc7a32dec5bf1e2771db1f0b6f6d9478e2e023e
Patricio Benavente <patricio.benavente@katoo.app>
2022-07-15 06:51:06 -0300

    ImagePicker

--- iTalk Chat/Support/Helpers/ImagePicker.swift
+++ iTalk Chat/Support/Helpers/ImagePicker.swift
@@ -19,6 +19,7 @@ struct ImagePicker: UIViewControllerRepresentable {
 	imagePicker.allowsEditing = false
 	imagePicker.sourceType = sourceType
 	imagePicker.delegate = context.coordinator
+    self.selectedImage = nil
 	return imagePicker
   }
 
